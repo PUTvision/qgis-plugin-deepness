2470a8775371d3898230fb819c69324605eca5d7
Bartosz Ptak <bartptak@gmail.com>
2022-09-09 12:16:32 +0200

    Add object detector model

---
+++ plugin/deep_segmentation_framework/processing/models/detector.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+
+from deep_segmentation_framework.processing.models.base import BaseModel
+
+
+class Detector(BaseModel):
+    def __init__(self, model_file_path: str):
+        super(Detector, self).__init__(model_file_path)
+
+        self.score_threshold = 0.6
+        self.iou_threshold = 0.5
+
+    def preprocessing(self, image: np.ndarray):
+        img = image[:, :, :self.input_shape[-3]]
+
+        input_data = (img / 255.0).astype(np.float32)
+        input_data = np.transpose(input_data, (2, 0, 1))
+        input_batch = np.expand_dims(input_data, 0)
+
+        return input_batch
+
+    def postprocessing(self, model_output):
+        model_output = model_output[0][0]
+
+        outputs_filtered = np.array(list(filter(lambda x: x[4] >= self.score_threshold, model_output)))
+
+        if len(outputs_filtered.shape) < 2:
+            return [], [], []
+
+        outputs_x1y1x2y2 = self.xywh2xyxy(outputs_filtered)
+
+        outputs_nms = self.non_max_suppression_fast(outputs_x1y1x2y2, self.iou_threshold)
+
+        boxes = np.int(outputs_nms[:, :4])
+        conf = outputs_nms[:, 4]
+        classes = np.argmax(outputs_nms[:, 5:], axis=1)
+
+        return boxes, conf, classes
+
+    @staticmethod
+    def xywh2xyxy(x):
+        y = np.copy(x)
+        y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+        y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+        y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+        y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+        return y
+
+    @staticmethod
+    def non_max_suppression_fast(boxes: np.ndarray, iou_threshold: float) -> np.ndarray:
+
+        # initialize the list of picked indexes
+        pick = []
+        # grab the coordinates of the bounding boxes
+        x1 = boxes[:, 0]
+        y1 = boxes[:, 1]
+        x2 = boxes[:, 2]
+        y2 = boxes[:, 3]
+        # compute the area of the bounding boxes and sort the bounding
+        # boxes by the bottom-right y-coordinate of the bounding box
+        area = (x2 - x1 + 1) * (y2 - y1 + 1)
+        idxs = np.argsort(y2)
+        # keep looping while some indexes still remain in the indexes
+        # list
+        while len(idxs) > 0:
+            # grab the last index in the indexes list and add the
+            # index value to the list of picked indexes
+            last = len(idxs) - 1
+            i = idxs[last]
+            pick.append(i)
+            # find the largest (x, y) coordinates for the start of
+            # the bounding box and the smallest (x, y) coordinates
+            # for the end of the bounding box
+            xx1 = np.maximum(x1[i], x1[idxs[:last]])
+            yy1 = np.maximum(y1[i], y1[idxs[:last]])
+            xx2 = np.minimum(x2[i], x2[idxs[:last]])
+            yy2 = np.minimum(y2[i], y2[idxs[:last]])
+            # compute the width and height of the bounding box
+            w = np.maximum(0, xx2 - xx1 + 1)
+            h = np.maximum(0, yy2 - yy1 + 1)
+            # compute the ratio of overlap
+            overlap = (w * h) / area[idxs[:last]]
+            # delete all indexes from the index list that have
+            idxs = np.delete(idxs, np.concatenate(([last],
+                                                   np.where(overlap > iou_threshold)[0])))
+
+        return boxes[pick]
