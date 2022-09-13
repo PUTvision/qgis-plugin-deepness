1a985e9d74818a09abff09c2a60bada30ef12d3f
Przemyslaw Aszkowski <przemyslaw.aszkowski@gmail.com>
2022-09-14 00:26:52 +0200

    x and y coordinates reversed in the processing

--- plugin/deep_segmentation_framework/processing/processing_utils.py
+++ plugin/deep_segmentation_framework/processing/processing_utils.py
@@ -193,8 +193,8 @@ def transform_points_list_xy_to_target_crs(
     points_crs = []
 
     for point_xy in points:
-        x_crs = point_xy[1] * rlayer_units_per_pixel + x_left
-        y_crs = -(point_xy[0] * rlayer_units_per_pixel - y_upper)
+        x_crs = point_xy[0] * rlayer_units_per_pixel + x_left
+        y_crs = -(point_xy[1] * rlayer_units_per_pixel - y_upper)
         points_crs.append(QgsPointXY(x_crs, y_crs))
     return points_crs
 
