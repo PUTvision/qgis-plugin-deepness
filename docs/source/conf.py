3c92133186ada34a664f49f3f236b418c302aa60
Bartosz Ptak <bartptak@gmail.com>
2022-10-11 23:35:59 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -67,6 +67,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
+    'sphinxcontrib.youtube',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
